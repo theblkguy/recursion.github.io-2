17898009f0fc5abcbfbd007813375ff24d220507
theblkguy <allblk13@gmail.com>
2025-02-10 21:27:45 -0600

    sum [complete]

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -17,13 +17,18 @@ var factorial = function(n) {
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
-var sum = function(array) {
+var sum = (array) => {
+  if(array.length === 0){
+    return 0
+  } else {
+    return array[0] + sum(array.slice(1))
+  }
 };
 
-// 3. Sum all numbers in an array containing nested arrays.
-// Example: arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
-};
+// // 3. Sum all numbers in an array containing nested arrays.
+// // Example: arraySum([1,[2,3],[[4]],5]); // 15
+// var arraySum = function(array) {
+// };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
