32292d67c8624ec3f5a202f879577321a46348de
theblkguy <allblk13@gmail.com>
2025-02-12 16:21:16 -0600

    range [attempt]

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -54,7 +54,14 @@ var sumBelow = function(n, output=0) {
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
-var range = function(x, y) {
+var range = function(x, y, output=[]) {
+  if(x > y){
+    return output
+  }
+  if(x <= y){
+    output.push(x + 1) 
+  }
+  return range(x + 1, y, output)
 };
 
 // 7. Compute the exponent of a number.
